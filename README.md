a591e297de0bf51091befa4f7ca6463d1aee3057
Hyunseok Lim <prlhspt@gmail.com>
2020-08-04 19:39:02 +0900

    test

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpringWebProject
+
+스프링 부트와 AWS로 게시판을 구현하는 웹 프로젝트 입니다..
---
+++ src/main/java/com/webproject/springboot/web/PostApiController.java
@@ -0,0 +1,36 @@
+package com.webproject.springboot.web;
+
+import com.webproject.springboot.service.posts.PostsService;
+import com.webproject.springboot.web.dto.PostsResponseDto;
+import com.webproject.springboot.web.dto.PostsSaveRequestDto;
+import com.webproject.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
+    }
+}
